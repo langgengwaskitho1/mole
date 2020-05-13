5bb3544d47a14172be78b9c2f8efaefbfd938532
Langgeng Waskitho <langgengwaskitho@gmail.com>
2020-05-13 23:29:33 +0700

    2142

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const tanah = document.querySelectorAll('.tanah');
+// query selector all digunakn untuk semua
+const tikus = document.querySelectorAll('.tikus');
+
+function munculkanTikus(tanah) {
+    tanah[1].classList.add('muncul');
+}
+
+
