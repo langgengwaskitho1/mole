807011e7ce8bfb48e76207762c07d22f8ebc4b0c
Langgeng Waskitho <langgengwaskitho@gmail.com>
2020-05-13 23:45:22 +0700

    2342

--- js/script.js
+++ js/script.js
@@ -2,8 +2,18 @@ const tanah = document.querySelectorAll('.tanah');
 // query selector all digunakn untuk semua
 const tikus = document.querySelectorAll('.tikus');
 
+function randomTanah(tanah) {
+    const tRandom = Math.floor(Math.random() * tanah.length);
+    // length = jumlah node list pada tanah
+    // florr untuk melakukan pembualatan kebawah.
+    // pembulatan ada ,florr(lantai kebawah) round(round pembulatan terdekat), ceil= pembulatan keatas
+    return tRandom;
+}
+
 function munculkanTikus(tanah) {
     tanah[1].classList.add('muncul');
+    // kelas index ke 1 kita mw tambhin kelas muncul
+    // dalam class list itu kita bsa mengecek, menambah atau menghapus elemen tertentu. 
 }
 
 
