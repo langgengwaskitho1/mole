cf0fc331f23c4e511dd79a2ac19f905925983cd2
Langgeng Waskitho <langgengwaskitho@gmail.com>
2020-05-14 11:23:49 +0700

    3152

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ let tanahSebelumnya;
 function randomTanah(tanah) {
     const t = Math.floor(Math.random() * tanah.length);
     const tRandom = tanah[t];
-    if( tRandom == tanahSebelumnya ) {
+    if (tRandom == tanahSebelumnya) {
         randomTanah(tanah);
     }
     tanahSebelumnya = tRandom;
@@ -16,18 +16,23 @@ function randomTanah(tanah) {
     // length = jumlah node list pada tanah
     // florr untuk melakukan pembualatan kebawah.
     // pembulatan ada ,florr(lantai kebawah) round(round pembulatan terdekat), ceil= pembulatan keatas
-    
+
     // kembalikan ketanah yang berapa
+}
+
+function randomWaktu(min, max) {
+    // untuk menentukan bilangan random dari berapa sampai berapa
+    return Math.round(Math.random() * (max - min) + min);
 }
 
 function munculkanTikus() {
     const tRandom = randomTanah(tanah);
+    const wRandom = randomWaktu(300, 100);
     tRandom.classList.add('muncul');
     // kelas index ke 1 kita mw tambhin kelas muncul
     // dalam class list itu kita bsa mengecek, menambah atau menghapus elemen tertentu. 
     setTimeout(() => {
         tRandom.classList.remove('muncul');
-    }, 500);
+        munculkanTikus();
+    }, wRandom);
 }
-
-
